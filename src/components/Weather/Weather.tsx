72224d57be20179efadbf8528deda0c29d903ede
maratdev0210 <maratforwork1@gmail.com>
2025-02-03 18:57:58 +0300

    static version of TemperatureComponent

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { SEO } from '..';
+import TemperatureComponent from './TemperatureComponent';
 
 const Weather: React.FC = () => {
   return (
     <div>
       <SEO title='Weather' />
+      <TemperatureComponent
+        size='large'
+        temperature={5}
+        optional={true}
+      />
+      {/*
       <div className='comingSoon'>
         <div>
           <h1 className='comingSoonText text text-center text-3xl sm:text-5xl md:text-7xl xl:text-9xl'>
             Coming Soon
           </h1>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
