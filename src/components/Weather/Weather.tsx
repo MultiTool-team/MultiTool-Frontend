ddfa5e628f2196a1a883e1a31786bcb6504a5bfe
danek52 <guskir3565@gmail.com>
2025-02-03 21:05:54 +0300

    add https, get coordinates

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { SEO } from '..';
+import GeoLocation from './GeoLocation.tsx';
 
 const Weather: React.FC = () => {
   return (
     <div>
       <SEO title='Weather' />
-      <div className='comingSoon'>
-        <div>
-          <h1 className='comingSoonText text text-center text-3xl sm:text-5xl md:text-7xl xl:text-9xl'>
-            Coming Soon
-          </h1>
-        </div>
-      </div>
+      <GeoLocation/>
+      {/*<div className='comingSoon'>*/}
+      {/*  <div>*/}
+      {/*    <h1 className='comingSoonText text text-center text-3xl sm:text-5xl md:text-7xl xl:text-9xl'>*/}
+      {/*     */}
+      {/*    </h1>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </div>
   );
 };
