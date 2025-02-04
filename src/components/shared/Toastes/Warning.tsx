c96c29e26d8e6bf34efc562b474cc99193890191
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-02-04 12:25:33 +0300

    add theme to toastes

--- src/components/shared/Toastes/Warning.tsx
+++ src/components/shared/Toastes/Warning.tsx
@@ -1,17 +1,26 @@
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { RootState } from '../../../store/store';
 
 interface IWarning {
   text: string;
 }
 
 const Warning: React.FC<IWarning> = ({ text }) => {
+  const darkMode = useSelector((state: RootState) => state.theme.darkMode);
+  console.log('Theme', darkMode);
   useEffect(() => {
     const notify = () => toast.warning(text);
     notify();
   }, [text]);
-  return <ToastContainer position='bottom-right' />;
+  return (
+    <ToastContainer
+      position='bottom-right'
+      theme={darkMode ? 'dark' : 'light'}
+    />
+  );
 };
 
 export default Warning;
