d19b3a8876fe4de7a658cb22d69054f1b2821a20
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-02-06 19:13:41 +0300

    add story

--- src/components/Weather/Temperature/Temperature.stories.tsx
+++ src/components/Weather/Temperature/Temperature.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Temperature from './Temperature';
 import '../../../index.css';
 import { StoriesTemplate } from '../..';
@@ -27,19 +28,27 @@ export default {
       defaultValue: 'light',
     },
   },
-};
+} as ComponentMeta<typeof Temperature>;
 
-const Template = args => {
-  return (
-    <StoriesTemplate theme={args.theme}>
-      <Temperature {...args} />
-    </StoriesTemplate>
-  );
-};
+interface TemperatureStoryProps {
+  size: 'small' | 'medium' | 'large';
+  temperature?: number;
+  feelsLikeTemperature?: number;
+  theme: 'light' | 'dark';
+}
+
+const Template: ComponentStory<typeof Temperature> = (
+  args: TemperatureStoryProps
+) => (
+  <StoriesTemplate theme={args.theme}>
+    <Temperature {...args} />
+  </StoriesTemplate>
+);
 
 export const TemperatureStories = Template.bind({});
 TemperatureStories.args = {
   size: 'medium',
   temperature: 52,
+  feelsLikeTemperature: undefined,
   theme: 'light',
 };
